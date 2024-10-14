e03a3870183de6f37884d2470042ab779456d93c
Harrison de Jong <harrisondijon@gmail.com>
2024-10-14 00:42:10 -0700

    Corrected filepaths in search and store html and js files

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-import { MESSAGES } from '../lang/messages/en/user.js';
+import { MESSAGES } from '/lang/messages/en/user.js';
 
 // Dynamically set user-facing strings
 document.addEventListener("DOMContentLoaded", function () {
